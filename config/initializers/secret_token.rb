f5bd5a8481cffa32723a203b895cb0f267cff82b
eatcpcks <yannick.schutz@gmail.com>
2011-01-20 22:14:07 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+P52::Application.config.secret_token = 'b02b14a973b22eda193038e5c8950d773c155de8ea45b45905432bf35b0531652df001b0ab09745505066a483766f6b3b3b7b77f94059cf233559ee62380e481'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+P52::Application.config.session_store :cookie_store, :key => '_p52_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# P52::Application.config.session_store :active_record_store
