1d8eab60fbfb37f101ab19b8296a8fa214ba1bdc
eatcpcks <yannick.schutz@gmail.com>
2011-01-28 11:02:00 +0100

    index

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -28,7 +28,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.xml
   def index
-    @projects = Project.asc(:title).find(:conditions =>{:user_id => @user.id})
+    @projects = Project.asc(:title).where(:user_id => @user.id)
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @projects }
