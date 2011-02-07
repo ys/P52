5d6a8d21ec5e6fc136d1737a601e899ad0406866
eatcpcks <yannick.schutz@gmail.com>
2011-02-07 15:41:28 +0100

    only a picture during the period

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -10,7 +10,7 @@ class PicturesController < ApplicationController
   before_filter :load_flickraw, :only=>[:create, :new, :edit, :update]
   before_filter :preload_new_edit, :only=>[:new, :create]
   before_filter :charge_image_from_params , :only => [:create]
-  #before_filter :verify_image! , :only=>[:create]
+  before_filter :verify_image! , :only=>[:create]
   require "kconv"
   def preload_new_edit
 
